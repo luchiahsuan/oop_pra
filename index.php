5d98e29f9d37a1a453e2d82b3dda6a81e6f0db56
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-12-16 10:57:20 +0800

    帶入實體化參數練習

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php
 
-$cat = new Animal;
 
-$dog = new Animal;
 
-echo $cat->type;
-echo "<br>";
-echo $dog->type;
-echo "<br>";
-echo $cat->name;
-echo "<br>";
-echo $dog->hair_color;
-echo "<br>";
-echo "<pre>";
-var_dump($cat);
-echo "</pre>";
+// echo $cat->type;
+// echo "<br>";
+// echo $dog->type;
+// echo "<br>";
+// echo $cat->name;
+// echo "<br>";
+// echo $dog->hair_color;
+// echo "<br>";
+// echo "<pre>";
+// var_dump($cat);
+// echo "</pre>";
+$cat = new Animal('小花','黑白相間');
+echo $cat->getName();
+echo $cat->getColor();
+$dog = new Animal('小米','扎染黑色');
+echo $dog->getName();
+echo $dog->getColor();
+
+
+
+
+
+
+$cat->run();
+$dog->speed();
 
 
 class Animal
@@ -23,12 +35,26 @@ class Animal
     public $name = 'John';
     public $hair_color = "brown";
 
-    public function __construct()
+    public function __construct($name,$color)
     {
+        $this->name = $name;
+        $this->hair_color = $color;
     }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getColor()
+    {
+        return $this->hair_color;
+    }
+
     public function run()
     {
         echo "我會跑喔!";
+        // $this->speed();
+        // echo$this->type;
     }
     private function speed()
     {
